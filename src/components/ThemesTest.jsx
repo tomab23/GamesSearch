3d1b21b4ada369e3e41c98ca6aac0ef7eb728d98
thomas bartier <thomas.bartier@gmail.com>
2024-04-22 11:51:33 +0200

    update V0 add theme

---
+++ src/components/ThemesTest.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const ThemesTest = ({ setTheme }) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+
+    const changeTheme = (theme) => {
+        setIsOpen(!isOpen);
+        setTheme(theme)
+    }
+
+    return (
+<div className="dropdown">
+  <div tabIndex={0} role="button" className="btn m-1" onClick={toggleDropdown}>Change Theme</div>
+    {isOpen && 
+      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 mr-20">
+        <li><button className='hover:underline' onClick={() => changeTheme("coffee")}>coffee</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("light")}>light</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("dark")}>dark</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("aqua")}>aqua</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("dracula")}>dracula</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("cyberpunk")}>cyberpunk</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("luxury")}>luxury</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("retro")}>retro</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("valentine")}>valentine</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("halloween")}>halloween</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("winter")}>winter</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("dim")}>dim</button></li>
+        <li><button className='hover:underline' onClick={() => changeTheme("sunset")}>sunset</button></li>
+    </ul>
+    }
+</div>
+      );
+}
+
+export default ThemesTest
+
+
+
+
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InProgress from '../components/InProgress'
+import ThemesTest from '../components/ThemesTest'
 
 const Home = () => {
+
+  const [theme, setTheme] = useState("coffee")
+
   return (
-    <div data-theme="coffee" className='min-h-screen text-center'>
+    <div data-theme={theme} className='min-h-screen text-center'>
+      <ThemesTest setTheme={setTheme} />
       <h1 className='text-9xl max-sm:text-6xl font-jersey'>GamesSearch</h1>
       <h2 className='text-2xl max-sm:text-xl font-jersey'>Information about your games</h2>
 
       <InProgress />
 
-      <p className='3xl:mt-72 xl:mt-56 max-xl:mt-72 max-sm:mt-64 italic font-jersey text-lg'>Application in development...</p>
+      <p className='3xl:mt-72 xl:mt-40 max-xl:mt-60 max-sm:mt-52 italic font-jersey text-lg'>Application in development...</p>
+      <p className='mt-1 italic font-jersey text-lg'>Coming in 2024</p>
     </div>
   )
 }
